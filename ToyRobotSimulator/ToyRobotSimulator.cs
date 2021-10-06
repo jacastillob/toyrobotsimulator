597367cd0e1cc5bdc77cffc569934ebb14db7f69
Jonathan Castillo <jacastillob@gmail.com>
2021-10-07 01:41:00 +1100

    Initial Commit

---
+++ ToyRobotSimulator/ToyRobotSimulator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ToyRobotSimulator
+{
+    public class ToyRobotSimulator
+    {
+        public ToyRobotSimulator()
+        {
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"> `PLACE 0,0,NORTH\nMOVE\nREPORT\n` </param>
+        /// <returns></returns>
+        public string run(string input)
+        {
+            // TODO: Parse and execute commands...
+
+            return "";
+        }
+    }
+}
---
+++ ToyRobotSimulator/test/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+namespace ToyRobotSimulator
+{   
+    [TestFixture()]
+    public class TestProgram
+    {
+        Program p = new Program();
+        [Test()]
+        public void Test0()
+        {
+            Assert.True(p.cont++ == 0);
+        }
+        
+    }
+}
